fcab026eb797f81afb7d20548370c455d856a1b4
modbender <{ID}+{username}@users.noreply.github.com>
2024-04-28 20:09:13 +0530

    tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,23 +3,37 @@ import { dirname, join } from "path";
 
 const currentDir = dirname(fileURLToPath(import.meta.url));
 
-console.log(currentDir);
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: ["@nuxt/content", "@nuxtjs/seo", "shadcn-nuxt", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxt/content",
+    "@nuxtjs/seo",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "shadcn-nuxt",
+  ],
 
   content: {
     documentDriven: true,
+  },
 
-    sources: {
-      docs: {
-        driver: "fs",
-        prefix: "/docs",
-        base: join(currentDir, ".playground", "docs"),
-      },
+  colorMode: {
+    preference: "dark",
+    classSuffix: "",
+  },
+
+  tailwindcss: {
+    cssPath: [
+      join(currentDir, "./assets/css/tailwind.css"),
+      { injectPosition: "first" },
+    ],
+    configPath: "tailwind.config",
+    exposeConfig: {
+      level: 2,
     },
+    config: {},
+    viewer: true,
   },
 });
